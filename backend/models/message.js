e4803dc103e5c6b1c676dd368d59621b78c31c54
conandg21 <conandg21@gmail.com>
2025-08-10 11:20:15 -0400

    Update message.js

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const MessageSchema = new mongoose.Schema({
+  carId: { type: mongoose.Schema.Types.ObjectId, ref: 'Car', required: false },
+  name: String,
+  email: String,
+  phone: String,
+  message: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
